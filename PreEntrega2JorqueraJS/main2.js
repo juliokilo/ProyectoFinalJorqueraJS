55434c832c83738c7bcd3ddc50b0d0a7cbe50aaf
juliopull <juliokilo14@gmail.com>
2023-12-06 12:18:38 -0300

    ProyectoFinalJorqueraJC

---
+++ PreEntrega2JorqueraJS/main2.js
@@ -0,0 +1,45 @@
+let participantes = [];
+
+function guardarParticipante() {
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = parseInt(document.getElementById("telefono").value);
+    let edad = parseInt(document.getElementById("edad").value);
+    nombre.value = "";
+    correo.value = "";
+    telefono.value = "";
+    edad.value = "";
+
+    if (nombre && edad && correo) {
+        if (edad >= 18) {
+            let participante = { nombre: nombre, correo: correo, telefono: telefono, edad: edad };
+            participantes.push(participante);
+            localStorage.setItem('participantes', JSON.stringify(participantes));
+            document.getElementById("mensaje").innerText = "¡Felicidades, usted está inscrito!";
+        } else {
+            document.getElementById("mensaje").innerText = "Lamentablemente debe ser mayor de 18 años para inscribirse";
+        }
+    } else {
+        document.getElementById("mensaje").innerText = "Por favor, complete todos los campos del formulario";
+    }
+    
+}
+
+function buscarParticipante() {
+    let nombreBusqueda = document.getElementById("buscarNombre").value;
+    let participantesGuardados = JSON.parse(localStorage.getItem('participantes')) || [];
+
+    let participanteEncontrado = participantesGuardados.find((participante) => {
+        return participante.nombre.toUpperCase() === nombreBusqueda.toUpperCase();
+    });
+
+    if (participanteEncontrado) {
+        let resultado = document.getElementById("resultado");
+        resultado.innerHTML = `<p>Nombre: ${participanteEncontrado.nombre}</p>
+                                <p>Correo: ${participanteEncontrado.correo}</p>
+                                <p>Teléfono: ${participanteEncontrado.telefono}</p>
+                                <p>Edad: ${participanteEncontrado.edad}</p>`;
+    } else {
+        document.getElementById("resultado").innerText = "No se encontró ningún participante con ese nombre.";
+    }
+}
